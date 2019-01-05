8de054df47e7ad5cdcbb7cff41a25014c204f7f1
sexfat <bryant6831@gmail.com>
2019-01-05 11:48:40 +0800

    add text

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -1,4 +1,3 @@
-
 // TweenMax.to('.box_01', 1, {
 //     x: 300,
 //     ease: Back.easeOut,
@@ -7,15 +6,41 @@
 
 // })
 
-
-
-
-TweenMax.fromTo('.box_01', .5, {
+TweenMax.fromTo('.box_01', 2, {
     x: 100,
     opacity: 0
 }, {
     x: 300,
     opacity: 1,
     ease: Back.easeOut,
-    scale: 1.5
+    scale: 1.5,
+    rotation: 360,
+    backgroundColor: '#f20',
+    repeat: 1,
+    repeatDelay: 1
+});
+
+TweenMax.set('.box_02', {
+    x: 200,
+    y: 100,
+    rotation: 90
+});
+
+
+var _btn = document.getElementById('btn');
+_btn.onclick = function () {
+    TweenMax.staggerFromTo('.box_03', 1, {
+        x: 0
+    }, {
+        x: 100,
+        ease: Elastic.easeOut
+    }, 0.3);
+};
+
+
+TweenMax.fromTo('.text-inner', .5, {
+    y: -100
+}, {
+    y: 0
+    // ease: Elastic.easeOut
 });
